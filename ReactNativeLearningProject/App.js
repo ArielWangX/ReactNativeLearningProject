557f223b99e93feba5955303cd617fa0c5e38de6
Ariel Wang <arielxiewang@gmail.com>
2023-12-06 17:27:22 +1100

    Build Game Page UI

--- ReactNativeLearningProject/App.js
+++ ReactNativeLearningProject/App.js
@@ -1,27 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Game from "./src/components/Game";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={[styles.text, {flex: 2}]}>Step 1</Text>
-      <Text style={[styles.text, {flex: 1}]}>Step 2</Text>
-      <Text style={[styles.text, {flex: 1}]}>Step 3</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Game randomNumberCount={6}/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "orange",
-    alignItems: "flex-start",
-    justifyContent: "space-around",
-  },
-  text: {
-    margin: 40,
-    backgroundColor: "red",
-  },
-});
